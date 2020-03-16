db3b544f5d10ead4e29490bc7118eb21eba826c9
liulm <luming.liu@dragonsoftbravo.com>
2020-03-16 11:40:54 +0800

    删除重配

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: [
     'airbnb',
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import MouseParallax from './module/MouseParallax';
-
-Object.defineProperty(window, 'effect', { value: MouseParallax });
-
-export default MouseParallax;
